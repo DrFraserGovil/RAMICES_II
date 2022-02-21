a4b039c4e1d11d10c082797f3b8608f73511009a
Jack Fraser <jackthomas.fraser@physics.ox.ac.uk>
2022-02-21 13:14:56 +0000

    Bulk upload of new files

---
+++ pollutantLaunch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#~ declare -a files=("dormant" "dormant_inflow" "quiescent" "active" "active_inflow" "active_diffusion")
+declare -a files=("active")
+dir="config"
+base=$dir"/pollute_base.config"
+for i in "${files[@]}"
+do
+	file="pollute_"$i".config"
+	loc=$dir"/"$file
+	out=$dir"/pollutant_composite_"$i".config"
+	cat $base $loc > $out
+	./RAMICES_II -config $out
+	#~ echo $i
+	echo $out
+	rm $out
+done
