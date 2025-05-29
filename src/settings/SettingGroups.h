1a00cb858f3181bd72205d90779273c0eb983761
Jack <jf20@sanger.ac.uk>
2025-05-29 09:18:01 +0100

    Updated the settings groups to include a few more subsets -- mostly placeholders for now, but needed to check the main holder entity

--- src/settings/SettingGroups.h
+++ src/settings/SettingGroups.h
@@ -1,13 +1,26 @@
 #pragma once
 
-#define SETTINGS_CATEGORY System
+#define SETTINGS_CATEGORY SystemSettings
 #define SETTINGS_FILE "settings_system.def"
 #define SETTINGS_VALIDATE
 #include "SettingsConstructor.h"
-void System::Validate()
+void SystemSettings::Validate()
 {
 	if (Verbosity > 3)
 	{
 		Verbosity.Value = 3;
 	}
 }
+
+#define SETTINGS_CATEGORY StellarSettings
+#define SETTINGS_FILE "settings_stellar.def"
+#include "SettingsConstructor.h"
+
+#define SETTINGS_CATEGORY ThermalSettings
+#define SETTINGS_FILE "settings_thermal.def"
+#include "SettingsConstructor.h"
+
+#define SETTINGS_CATEGORY YieldSettings
+#define SETTINGS_FILE "settings_thermal.def"
+#include "SettingsConstructor.h"
+
