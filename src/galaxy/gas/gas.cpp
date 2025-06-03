8f19d8cc26daaad2eeae778ffffcb21137a6d26e
Jack <jf20@sanger.ac.uk>
2025-06-03 16:50:29 +0100

    The gas object now has a robust internal cache and mutable section -- it's similar to the old one, but more advanced

--- src/galaxy/gas/gas.cpp
+++ src/galaxy/gas/gas.cpp
@@ -1,100 +1,186 @@
 #include "gas.h"
 
 using namespace Element;
-Gas::Gas() : internalMassOf(Element::Count,0.0)
+Gas::Gas() : internalMassOf(Element::Count,0.0), internalComposition(Element::Count,0.0)
 {
 	internal_Mass = 0;
 }
 
-double Gas::Mass()
-{
-	if (NeedsRecomputing)
+
+//Property Queries
+	double Gas::Mass() const
 	{
-		ComputeMass();
+		if (massNeedsRecomputing)
+		{
+			ComputeMass();
+		}
+		return internal_Mass;
 	}
-	return internal_Mass;
-}
 
-double Gas::Metallicity()
-{
-	double totalMass = Mass();
-	double nonMetal = internalMassOf[Hydrogen] + internalMassOf[Helium];
-	return 1.0 - nonMetal/totalMass;
-}
-
-
-
-double & Gas::operator[](Element::Species id)
-{
-	NeedsRecomputing= true;//returning a reference means the object may have been modified
-	return internalMassOf[id];
-}
-
-double & Gas::operator[](int id)
-{
-	NeedsRecomputing= true;//returning a reference means the object may have been modified
-	return internalMassOf[id];
-}
-double Gas::MassOf(Element::Species id)
-{
-	return internalMassOf[id];
-}
-double Gas::FractionOf(Element::Species id)
-{
-	double m = Mass();
-	if (m== 0)
+	double Gas::Metallicity() const
 	{
-		return 0;
+		double nonMetal = FractionOf(Hydrogen) + FractionOf(Helium);
+		return 1.0 - nonMetal;
 	}
-	return internalMassOf[id]/m;
-}
 
-void Gas::ComputeMass()
-{
-	internal_Mass = 0;
-	for (int i =0; i < Element::Count; ++i)
+
+	double Gas::MassOf(Element::Species id) const
 	{
-		internal_Mass += internalMassOf[i];
+		return internalMassOf[id];
 	}
-	NeedsRecomputing = false;
-}
+	double Gas::FractionOf(Element::Species id) const
+	{
+		return Composition()[id];
+	}
 
+	const std::vector<double> & Gas::Composition() const
+	{
+		if (compNeedsRecomputing)
+		{
+			ComputeComposition();
+		}
+		return internalComposition;
+	}
+
+//Access and modifications 
+	double & Gas::operator[](Element::Species id)
+	{
+		RegisterChanges();//returning a reference means the object may have been modified
+		return internalMassOf[id];
+	}
+
+	double & Gas::operator[](int id)
+	{
+		RegisterChanges();//returning a reference means the object may have been modified
+		return internalMassOf[id];
+	}
+	double Gas::operator[](Element::Species id) const
+	{
+		return internalMassOf[id];
+	}
+
+	double Gas::operator[](int id) const
+	{
+		return internalMassOf[id];
+	}
+
+	void Gas::Absorb(const Gas & input)
+	{
+		for (int i = 0; i < Element::Count; ++i)
+		{
+			internalMassOf[i] += input.internalMassOf[i];
+		}
+		RegisterChanges();
+	}
+
+	void Gas::DepleteByFraction(double frac)
+	{
+		if (frac < 0 || frac > 1)
+		{
+			LOG(ERROR) << "Cannot deplete gas by a fraction '" << frac << "', values must be in range [0,1]";
+			throw std::logic_error("Unphysical quantity encountered");
+		}
+		double remainingFraction = 1.0 - frac;
+		for (int i = 0; i < Element::Count; ++i)
+		{
+			internalMassOf[i] *= remainingFraction;
+		}
+		RegisterChanges();
+	}
+	void Gas::DepleteByMass(double mass)
+	{
+		double m = Mass();
+		if (m == 0)
+		{
+			if (mass == 0)
+			{
+				return;
+			}
+			LOG(ERROR) << "Cannot deplete a non-zero amount of gas from an empty gas reservoir";
+			throw std::logic_error("Unphysical quantity encountered");
+		}
+		double depletionFraction = mass/Mass();
+		DepleteByFraction(depletionFraction);
+	}
+
+//Internal functions
+	void Gas::RegisterChanges()
+	{
+		massNeedsRecomputing = true;
+		compNeedsRecomputing = true;
+	}
+	void Gas::ComputeMass() const
+	{
+		internal_Mass = 0;
+		for (int i =0; i < Element::Count; ++i)
+		{
+			internal_Mass += internalMassOf[i];
+		}
+		massNeedsRecomputing = false;
+	}
+	void Gas::ComputeComposition() const
+	{
+		double m = Mass();
+		for (int i =0; i < Element::Count; ++i)
+		{
+			if (m == 0)
+			{
+				internalComposition[i] = 0;
+			}
+			else
+			{
+				internalComposition[i] = internalMassOf[i]/m;
+			}
+		}
+		compNeedsRecomputing = false;
+	}
+	
 
 //Factories
-Gas Gas::WithComposition(double mass,const std::vector<double> & composition)
-{
-	if (mass < 0)
+	Gas Gas::WithComposition(double mass,const std::vector<double> & composition)
 	{
-		LOG(ERROR) << "Cannot create a gas object with negative mass (" << mass << " < 0)";
-		throw std::logic_error("Unphysical quantity encountered");
+		if (mass < 0)
+		{
+			LOG(ERROR) << "Cannot create a gas object with negative mass (" << mass << " < 0)";
+			throw std::logic_error("Unphysical quantity encountered");
+		}
+		if (composition.size() != Element::Count)
+		{
+			LOG(ERROR) << "Partial composition vectors are invalid. Cannot construct a gas object without a correctly sized composition";
+			throw std::runtime_error("Incorrectly sized composition vector (" + std::to_string(composition.size()) + ")");
+		}
+		auto g = Gas();
+		for (int el = 0; el < Element::Count; ++el)
+		{
+			g[el] = composition[el] * mass;
+		}
+		g.massNeedsRecomputing = true;
+		return g;
 	}
-	auto g = Gas();
-	for (int el = 0; el < Element::Count; ++el)
+
+	Gas Gas::WithMass(const std::vector<double> & massArray)
 	{
-		g[el] = composition[el] * mass;
+		auto g = Gas();
+		for (int el = 0; el < Element::Count; ++el)
+		{
+			g[el] = massArray[el];
+		}
+		g.massNeedsRecomputing = true;
+		return g;
 	}
-	g.NeedsRecomputing = true;
-	return g;
-}
 
-Gas Gas::WithMass(const std::vector<double> & massArray)
-{
-	auto g = Gas();
-	for (int el = 0; el < Element::Count; ++el)
+	Gas Gas::Primordial(double mass)
 	{
-		g[el] = massArray[el];
+		return Gas::WithComposition(mass,Settings.Abundance.PrimordialAbundances);
 	}
-	g.NeedsRecomputing = true;
-	return g;
-}
 
-Gas Gas::Primordial(double mass)
-{
-	return Gas::WithComposition(mass,Settings.Abundance.PrimordialAbundances);
-}
+	Gas Gas::WithSameComposition(double mass, const Gas & targetGas)
+	{
+		return Gas::WithComposition(mass,targetGas.Composition());
+	}
 
-Gas Gas::Empty()
-{
-	auto g =  Gas();
-	return g;
-}
+	Gas Gas::Empty()
+	{
+		auto g =  Gas();
+		return g;
+	}
--- src/galaxy/gas/gas.h
+++ src/galaxy/gas/gas.h
@@ -13,27 +13,33 @@ class Gas
 		//! Create a gas object with a specified mass, and elemental composition   \param mass the final mass of the gas object \param composition A fractional array of length Element::Count which specifies the fraction of the total mass composed of each corresponding element. \returns A gas object with a specified mass and fractional composition
 		static Gas WithComposition(double mass, const std::vector<double> & composition);
 
+		static Gas WithSameComposition(double mass, const Gas & targetGas);
+
 		//! Creates the object with a direct copy of the mas grid. The final mass is equal to the sum of massArray.
 		static Gas WithMass(const std::vector<double> & massArray);
 		// 
 		//! \return A gas object of the specified mass but with a primordial elemental abundance distribution (X = 0.75, Y = 0.25 etc)
 		static Gas Primordial(double mass);
 	
-		
+		const std::vector<double> & Composition() const;
 
-		
+		void Absorb(const Gas & input);
+		void DepleteByFraction(double amount);
+		void DepleteByMass(double amount);
 	
 		//! \return The current total mass within the #Species array
-		double Mass();
+		double Mass() const;
 		
 		//! \return The current metallicity
-		double Metallicity();
+		double Metallicity() const;
 		
-		double FractionOf(Element::Species id);
-		double MassOf(Element::Species id);
+		double FractionOf(Element::Species id) const;
+		double MassOf(Element::Species id) const;
 		//! \return A reference to the indexed member of #Species, allowing for vector like access
 		double & operator[](Element::Species id);
 		double & operator[](int id);
+		double  operator[](Element::Species id) const;
+		double  operator[](int id) const;
 		
 	
 		// double Mass() const;
@@ -47,10 +53,17 @@ class Gas
 
 		//! The central mass array. Has ::ElementCount elements, indexed by ElementID
 		std::vector<double> internalMassOf;
-	
-		void ComputeMass();
 		
-		bool NeedsRecomputing;
-		double internal_Mass;
+		void ComputeMass() const;
+		void ComputeComposition() const;
+		void RegisterChanges();
+
+		//caches declared mutable so that const Gas doesn't return nonsense values
+		//mutable means that they are non-const even in a const. object
+		//that's fine here because they're internal caches
+		mutable bool massNeedsRecomputing;
+		mutable bool compNeedsRecomputing;
+		mutable std::vector<double> internalComposition;
+		mutable double internal_Mass;
 	
 };
